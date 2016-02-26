1e8b628e01e23a5f5c2ffbe8e248798934a99cb0
Shawn Deleurme <shawn@deleur.me>
2016-02-25 19:28:20 -0800

    Fixed oversight.

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 
 OS=`uname`
 
-if [ $OS == "Darwin" -a -x `which brew` ]; then
+if [ $OS == "Darwin" ] && [  -x $(which brew)  ]; then 
   cp tugboat_completion /usr/local/etc/bash_completion.d
 else 
   cp tugboat_completion /etc/bash_completion.d/
